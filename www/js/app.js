9e39261900446d92602ca000195705b1035071d5
Antonio Manuel Pérez López <aperezl@gmail.com>
2015-07-30 17:04:44 +0200

    Autocommit

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,10 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'ngCordova'])
+angular.module('starter', ['ionic', 'ngCordova', 'starter.service'])
 
-.run(function($ionicPlatform) {
+
+.run(function($ionicPlatform, $cordovaGeolocation, geoLocation) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -15,9 +16,75 @@ angular.module('starter', ['ionic', 'ngCordova'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
-    console.log('inicio')
+
+    $cordovaGeolocation
+    .getCurrentPosition()
+    .then(function(position) {
+        geoLocation.setGeolocation(position.coords.latitude, position.coords.longitude)
+    }, function(err) {
+        geoLocation.setGeolocation(37.38, -122.09)
+    });
+
+      // begin a watch
+      var options = {
+          frequency: 1000,
+          timeout: 3000,
+          enableHighAccuracy: true
+      };
+
+      var watch = $cordovaGeolocation.watchPosition(options);
+      console.log(watch.then)
+      watch.then(function() { /* Not  used */ },
+          function(err) {
+              geoLocation.setGeolocation(37.38, -122.09)
+          },
+          function(position) {
+              geoLocation.setGeolocation(position.coords.latitude, position.coords.longitude)
+          });
+
+
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('lista', {
+    url: '/lista',
+    views: {
+      lista: {
+        templateUrl: 'lista.html'
+      }
+    }
+  });
+  $stateProvider.state('detalle', {
+     url: '/station/:id',
+    views: {
+      detalle: {
+        templateUrl: 'detalle.html'
+      }
+    }
+  });
+
+  $stateProvider.state('home', {
+    url: '/home',
+    views: {
+      home: {
+        templateUrl: 'home.html'
+      }
+    }
+  });
+
+  $stateProvider.state('help', {
+    url: '/help',
+    views: {
+      help: {
+        templateUrl: 'help.html'
+      }
+    }
+  });
+
+  $urlRouterProvider.otherwise('/lista');
+})
+
 
 .factory('listaService', ['$rootScope', function($rootScope) {
   var service = {
@@ -42,43 +109,63 @@ angular.module('starter', ['ionic', 'ngCordova'])
   return service;
 }])
 
-.controller('mainCtrl', function($scope, $rootScope, listaService, $http, $cordovaGeolocation) {
+.controller('mainCtrl', function($scope, $rootScope, listaService, $http, $cordovaGeolocation, geoLocation, $state) {
   console.log('iniciado el controlador');
 
 
 
 
-  $scope.dist = function(lat1, lat2, lon1, lon2) {
-    function deg2rad(deg) {
-      return deg * (Math.PI/180);
+  $scope.distance = function(lat2, lon2) {
+    var pos = geoLocation.getGeolocation();
+    var lat1 = pos.lat;
+    var lon1 = pos.lng;
+    var radlat1 = Math.PI * lat1/180
+    var radlat2 = Math.PI * lat2/180
+    var radlon1 = Math.PI * lon1/180
+    var radlon2 = Math.PI * lon2/180
+    var theta = lon1-lon2
+    var radtheta = Math.PI * theta/180
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist)
+    dist = dist * 180/Math.PI
+    dist = dist * 60 * 1.1515
+    dist = dist * 1.609344;
+    if(dist>1) {
+        dist = dist.toFixed(2) + ' Km';
+    } else {
+        dist = parseInt(dist * 1000) + ' m';
     }
-    var R = 6371;
-    var dLat = deg2rad(lat2-lat1);
-    var dLon = deg2rad(lon2-lon1);
-    var a =
-      Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) +
-      Math.sin(dLon/2) * Math.sin(dLon/2);
-
-    //var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    //var d = R * c;
-
-    console.log('Math.sqrt(a)', Math.sqrt(a));
-    console.log('Math.asin(x)', Math.asin(a));
-    //return d;
-
-    return R * 2 * Math.asin(Math.sqrt(a));
+    return dist
   }
 
-  $scope.accion = function(a) {
-    console.log(a);
+  $scope.accion = function(id) {
+    console.log(id);
+    $http.get('http://unuko.com:7777/gas/' + id)
+    .then(function(station) {
+      console.log('success', station);
+      $state.go('detalle')
+    }, function(err) {
+      console.error('ERR', err);
+    })
   }
 
   $scope.prueba = function() {
-    alert('esto es una prueba');
+    var pos = geoLocation.getGeolocation();
+    $http.get('http://unuko.com:7777/gas?lat=' + pos.lat + '&lon=' + pos.lng)
+      .then(function(stations) {
+        console.log('success', stations);
+        listaService.model.lista = stations.data;
+        listaService.SaveState();
+        $scope.lista = listaService.model.lista;
+        $state.go('lista')
+      }, function(err) {
+        console.log('ERR', err);
+      });
+
   }
 
   $scope.getStations = function() {
+    console.log('getStations')
     var posOptions = {timeout: 10000, enableHighAccuracy: false};
     $cordovaGeolocation
     .getCurrentPosition(posOptions)
@@ -110,5 +197,5 @@ angular.module('starter', ['ionic', 'ngCordova'])
     })
   }
 
-  console.log($scope.dist(37.345021, -4.93425, 37.345028, -5.93425))
+  //console.log($scope.dist(37.345021, -4.93425, 37.345028, -5.93425))
 });
---
+++ www/js/services.js
@@ -0,0 +1,56 @@
+angular.module('starter.service', ['ionic', 'ngCordova'])
+
+.factory('$localStorage', ['$window', function ($window) {
+    return {
+        set: function (key, value) {
+            $window.localStorage[key] = value;
+        },
+        get: function (key, defaultValue) {
+            return $window.localStorage[key] || defaultValue;
+        },
+        setObject: function (key, value) {
+            $window.localStorage[key] = JSON.stringify(value);
+        },
+        getObject: function (key) {
+            return JSON.parse($window.localStorage[key] || '{}');
+        }
+    }
+}])
+.factory('geoLocation', function($localStorage) {
+    return {
+        setGeolocation: function(latitude, longitude) {
+            var _position = {
+                latitude: latitude,
+                longitude: longitude
+            }
+            $localStorage.setObject('geoLocation', _position)
+        },
+        getGeolocation: function() {
+            return glocation = {
+                lat: $localStorage.getObject('geoLocation').latitude,
+                lng: $localStorage.getObject('geoLocation').longitude
+            }
+        },
+        getDistance: function(lat2, lon2) {
+            var lat1 = $localStorage.getObject('geoLocation').latitude;
+            var lon1 = $localStorage.getObject('geoLocation').longitude;
+            var radlat1 = Math.PI * lat1/180
+            var radlat2 = Math.PI * lat2/180
+            var radlon1 = Math.PI * lon1/180
+            var radlon2 = Math.PI * lon2/180
+            var theta = lon1-lon2
+            var radtheta = Math.PI * theta/180
+            var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+            dist = Math.acos(dist)
+            dist = dist * 180/Math.PI
+            dist = dist * 60 * 1.1515
+            dist = dist * 1.609344;
+            if(dist>1) {
+                dist = dist.toFixed(2) + ' Km';
+            } else {
+                dist = parseInt(dist * 1000) + ' m';
+            }
+            return dist
+        }
+    }
+})
